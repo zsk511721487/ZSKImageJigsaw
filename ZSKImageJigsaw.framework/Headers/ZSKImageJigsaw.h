38a841cffb01beb3d8fb0588a0ed21ae4c477109
张少康 <511721487@qq.com>
2021-02-25 22:07:53 +0800

    create SKImageJigsawSDK

---
+++ ZSKImageJigsaw.framework/Headers/ZSKImageJigsaw.h
@@ -0,0 +1,18 @@
+//
+//  ZSKImageJigsaw.h
+//  ZSKImageJigsaw
+//
+//  Created by 张少康 on 2021/2/22.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for ZSKImageJigsaw.
+FOUNDATION_EXPORT double ZSKImageJigsawVersionNumber;
+
+//! Project version string for ZSKImageJigsaw.
+FOUNDATION_EXPORT const unsigned char ZSKImageJigsawVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZSKImageJigsaw/PublicHeader.h>
+
